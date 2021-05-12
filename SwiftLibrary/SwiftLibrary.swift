93bd015537b6e7dfbc01f3a1de7e1a5c2929512f
AnjaliJariwala <anj@narola.email>
2021-05-12 14:19:59 +0530

    Add Test Cases

---
+++ SwiftLibrary/SwiftLibrary.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftLibrary.swift
+//  SwiftLibrary
+//
+//  Created by Anjali Jariwala on 12/05/21.
+//
+
+import Foundation
+public class SwiftLibrary{
+    let name = "Swift Library"
+    
+    public func add(a : Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func subtract(a : Int, b : Int) -> Int{
+        return a - b
+    }
+}
--- SwiftLibraryTests/SwiftLibraryTests.swift
+++ SwiftLibraryTests/SwiftLibraryTests.swift
@@ -10,6 +10,21 @@ import XCTest
 
 class SwiftLibraryTests: XCTestCase {
 
+    var swiftLibrary = SwiftLibrary()
+    
+//    override class func setUp() {
+//        swiftLibrary = SwiftLibrary()
+//    }
+    func testAdd()
+    {
+        XCTAssertEqual(swiftLibrary.add(a: 1, b: 2), 3)
+    }
+    
+    func testSubtract()
+    {
+        XCTAssertEqual(swiftLibrary.subtract(a: 2, b: 1), 1)
+    }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
